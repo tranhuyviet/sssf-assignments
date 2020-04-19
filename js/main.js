1765c88d882e9b7487dfe867a60c434fd8d278bb
Viet Tran <tranhuyviet@gmail.com>
2020-04-19 13:18:40 +0300

    part 2 - send greetings to server with background sync

--- js/main.js
+++ js/main.js
@@ -1,14 +1,54 @@
-window.onload = () => {
-    'use strict';
+'use strict';
 
-    (async () => {
-        if ('serviceWorker' in navigator) {
-            try {
-                const worker = await navigator.serviceWorker.register('./sw.js');
-                console.log('Service Worker Registered');
-            } catch (e) {
-                console.log(e.message);
+window.addEventListener('load', async () => {
+    const ul = document.querySelector('ul');
+    const rfrsh = document.querySelector('#refresh');
+    const form = document.querySelector('form');
+    const username = 'viett';
+    const greeting = form.elements.greeting;
+
+    if ('serviceWorker' in navigator) {
+        try {
+            await navigator.serviceWorker.register('./sw.js');
+            const registration = await navigator.serviceWorker.ready;
+            if ('sync' in registration) {
+                form.addEventListener('submit', async (event) => {
+                    event.preventDefault();
+                    const message = {
+                        username,
+                        greeting: greeting.value,
+                    };
+                    try {
+                        saveData('outbox', message);
+                        await registration.sync.register('send-message');
+                    } catch (e) {
+                        console.log(e.message);
+                    }
+                });
             }
+        } catch (e) {
+            console.log(e.message);
         }
-    })();
-};
+    }
+
+    const init = async () => {
+        const data = [];
+        try {
+            const greetings = await getGreetingsByUser(username);
+            for (const message of greetings) {
+                data.push(message);
+            }
+        } catch (e) {
+            console.log(e.message);
+        }
+
+        ul.innerHTML = '';
+        data.forEach((item) => {
+            ul.innerHTML += `<li>${item.username}: ${item.greeting}</li>`;
+        });
+    };
+
+    init();
+
+    rfrsh.addEventListener('click', init);
+});
--- sw.js
+++ sw.js
@@ -1,31 +1,62 @@
-var cacheName = 'hello-pwa';
-var filesToCache = ['/', '/index.html', '/css/style.css', '/js/main.js'];
+'use strict';
+self.importScripts('./js/fetchGQL.js');
+self.importScripts('./js/idb.js');
+const cacheName = 'hello-pwa';
+const filesToCache = [
+    './',
+    './index.html',
+    './favicon.ico',
+    './css/style.css',
+    './js/main.js',
+    './js/idb.js',
+    './images/pwa-bg.jpg',
+];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', (event) => {
-    event.waitUntil(
+self.addEventListener('install', (e) => {
+    e.waitUntil(
         (async () => {
             try {
-                const cache = await caches.open('v1');
-                return cache.addAll(arrayOfYourStaticFiles);
+                const cache = await caches.open(cacheName);
+                // console.log(cache);
+                return cache.addAll(filesToCache);
             } catch (e) {
-                console.log(e.message);
+                console.log('after install', e.message);
             }
         })()
     );
 });
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', (event) => {
-    console.log('ServiceWorker Fetch', event.request.url);
-    event.respondWith(
+self.addEventListener('fetch', (e) => {
+    // console.log(e.request);
+    e.respondWith(
         (async () => {
             try {
-                const response = await caches.match(event.request);
-                return response || fetch(event.request);
+                const response = await caches.match(e.request);
+                // console.log('resp', response);
+                return response || fetch(e.request);
             } catch (e) {
-                console.log(e.message);
+                console.log('load cache', e.message);
             }
         })()
     );
 });
+
+self.addEventListener('sync', (event) => {
+    if (event.tag == 'send-message') {
+        event.waitUntil(sendToServer());
+    }
+});
+
+const sendToServer = async () => {
+    try {
+        const outbox = await loadData('outbox');
+        console.log('outbox', outbox);
+        const sentMessages = outbox.map(async (message) => await saveGreeting(message));
+        console.log('sentMessages', sentMessages);
+        clearData('outbox');
+    } catch (e) {
+        console.log(e.message);
+    }
+};
